fa440fb79dbdd8b269b0b10f0d6f6e22e4038770
Fernando Herrera <fernando.herrera85@gmail.com>
2025-01-21 11:19:25 -0500

    add product card component, implement product fetching, and enhance home page layout

--- src/app/store-front/pages/home-page/home-page.component.ts
+++ src/app/store-front/pages/home-page/home-page.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { ProductCardComponent } from '@products/components/product-card/product-card.component';
+import { ProductsService } from '@products/services/products.service';
+// import { ProductCardComponent } from '../../../products/components/product-card/product-card.component';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [ProductCardComponent],
   templateUrl: './home-page.component.html',
 })
-export class HomePageComponent {}
+export class HomePageComponent {
+  productsService = inject(ProductsService);
+
+  productsResource = rxResource({
+    request: () => ({}),
+    loader: ({ request }) => {
+      return this.productsService.getProducts({
+        limit: 5,
+        gender: 'women',
+      });
+    },
+  });
+}
