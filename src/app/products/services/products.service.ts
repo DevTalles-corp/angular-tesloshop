de4086770760baebeb04d0925f2023b7b6ce182d
Fernando Herrera <fernando.herrera85@gmail.com>
2025-01-28 14:30:37 -0500

    enhance product details component; implement image upload functionality and carousel updates; improve image handling in product service

--- src/app/products/services/products.service.ts
+++ src/app/products/services/products.service.ts
@@ -6,7 +6,7 @@ import {
   Product,
   ProductsResponse,
 } from '@products/interfaces/product.interface';
-import { delay, Observable, of, pipe, tap } from 'rxjs';
+import { delay, forkJoin, map, Observable, of, pipe, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 const baseUrl = environment.baseUrl;
@@ -112,5 +112,27 @@ export class ProductsService {
     });
 
     console.log('Caché actualizado');
+  }
+
+  // Tome un FileList y lo suba
+  uploadImages(images?: FileList): Observable<string[]> {
+    if (!images) return of([]);
+
+    const uploadObservables = Array.from(images).map((imageFile) =>
+      this.uploadImage(imageFile)
+    );
+
+    return forkJoin(uploadObservables).pipe(
+      tap((imageNames) => console.log({ imageNames }))
+    );
+  }
+
+  uploadImage(imageFile: File): Observable<string> {
+    const formData = new FormData();
+    formData.append('file', imageFile);
+
+    return this.http
+      .post<{ fileName: string }>(`${baseUrl}/files/product`, formData)
+      .pipe(map((resp) => resp.fileName));
   }
 }
